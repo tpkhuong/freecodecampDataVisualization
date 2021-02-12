d4c719afeea1e0b101c3d7ae25f178cc3550ab83
Toan <tpkhuong@gmail.com>
2021-02-12 08:10:31 -0500

    js code for freecome d3 module

--- dataVisualizationWithD3/d3App.js
+++ dataVisualizationWithD3/d3App.js
@@ -362,3 +362,125 @@ svg
   .attr("cy", (d) => h - d[1])
   .attr("r", 5);
 // Add your code above this line
+
+const dataset = [
+  [34, 78],
+  [109, 280],
+  [310, 120],
+  [79, 411],
+  [420, 220],
+  [233, 145],
+  [333, 96],
+  [222, 333],
+  [78, 320],
+  [21, 123],
+];
+
+const w = 500;
+const h = 500;
+
+const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+
+svg
+  .selectAll("circle")
+  .data(dataset)
+  .enter()
+  .append("circle")
+  .attr("cx", (d, i) => d[0])
+  .attr("cy", (d, i) => h - d[1])
+  .attr("r", 5);
+
+svg
+  .selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  // Add your code below this line
+  .attr("x", (d) => d[0] + 5)
+  .attr("y", (d) => h - d[1])
+  .text((d) => d[0] + "," + d[1]);
+// Add your code above this line
+
+const positionData = [
+  [1, 7, -4],
+  [6, 3, 8],
+  [2, 9, 3],
+];
+// Add your code below this line
+
+const output = d3.min(positionData, (d) => d[2]); // Change this line
+
+// Add your code above this line
+
+d3.select("body").append("h2").text(output);
+
+const dataset = [
+  [34, 78],
+  [109, 280],
+  [310, 120],
+  [79, 411],
+  [420, 220],
+  [233, 145],
+  [333, 96],
+  [222, 333],
+  [78, 320],
+  [21, 123],
+];
+
+const w = 500;
+const h = 500;
+const padding = 60;
+
+const xScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(dataset, (d) => d[0])])
+  .range([padding, w - padding]);
+
+const yScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(dataset, (d) => d[1])])
+  .range([h - padding, padding]);
+
+const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+
+svg
+  .selectAll("circle")
+  .data(dataset)
+  .enter()
+  .append("circle")
+  .attr("cx", (d) => xScale(d[0]))
+  .attr("cy", (d) => yScale(d[1]))
+  .attr("r", (d) => 5);
+
+svg
+  .selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text((d) => d[0] + "," + d[1])
+  .attr("x", (d) => xScale(d[0] + 10))
+  .attr("y", (d) => yScale(d[1]));
+
+const xAxis = d3.axisBottom(xScale);
+// Add your code below this line
+const yAxis = undefined;
+// Add your code above this line
+
+svg
+  .append("g")
+  .attr("transform", "translate(0," + (h - padding) + ")")
+  .call(xAxis);
+
+// Add your code below this line
+var yAxis = d3.axisLeft(yScale);
+
+svg.append("g").attr("transform");
+svg
+  .append("g")
+  .attr("transform", "translate(" + padding + ",0")
+  .call(yAxis);
+svg
+  .append("g")
+  .attr("transform", "translate(" + padding + ",0)")
+  .call(yAxis);
+// Add your code above this line
